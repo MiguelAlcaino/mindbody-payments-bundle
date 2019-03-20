19cbe1b65d4c1a107e2cb223eafbe4305d9d02fd
Miguel Alcaino <miguel.alcaino@ogb.cl>
2019-03-20 01:52:42 +0100

    checkoutShoppingCart method added to SaleServiceSOAPRequest.php

--- src/Service/MindbodySOAPRequest/SaleServiceSOAPRequest.php
+++ src/Service/MindbodySOAPRequest/SaleServiceSOAPRequest.php
@@ -2,6 +2,7 @@
 
 namespace MiguelAlcaino\MindbodyPaymentsBundle\Service\MindbodySOAPRequest;
 
+use MiguelAlcaino\MindbodyPaymentsBundle\Service\MindbodySOAPRequest\Request\SaleService\CheckoutShoppingCartRequest;
 use MiguelAlcaino\MindbodyPaymentsBundle\Service\MindbodySOAPRequest\Request\SaleService\GetServicesRequest;
 
 class SaleServiceSOAPRequest extends AbstractSOAPRequester
@@ -21,6 +22,25 @@ class SaleServiceSOAPRequest extends AbstractSOAPRequester
         $response = $this->minbodySoapRequester->createEnvelopeAndExecuteRequest(
             self::SERVICE_URI,
             'GetServices',
+            $arrayRequest
+        );
+
+        return $response;
+    }
+
+    /**
+     * @param CheckoutShoppingCartRequest $request
+     *
+     * @return array
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function checkoutShoppingCart(CheckoutShoppingCartRequest $request): array
+    {
+        $arrayRequest = $this->decodeRequesterObject($request);
+
+        $response = $this->minbodySoapRequester->createEnvelopeAndExecuteRequest(
+            self::SERVICE_URI,
+            'CheckoutShoppingCart',
             $arrayRequest
         );
 
