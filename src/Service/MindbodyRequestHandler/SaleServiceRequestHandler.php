8b9511ae32eb44e4abe6b3a135214d3b1bb967df
Miguel Alcaino <miguel.alcaino@ogb.cl>
2019-03-20 01:53:12 +0100

    calculateShoppingCart method added to SaleServiceRequestHandler

--- src/Service/MindbodyRequestHandler/SaleServiceRequestHandler.php
+++ src/Service/MindbodyRequestHandler/SaleServiceRequestHandler.php
@@ -2,7 +2,9 @@
 
 namespace MiguelAlcaino\MindbodyPaymentsBundle\Service\MindbodyRequestHandler;
 
+use MiguelAlcaino\MindbodyPaymentsBundle\Service\MindbodySOAPRequest\Request\SaleService\CheckoutShoppingCartRequest;
 use MiguelAlcaino\MindbodyPaymentsBundle\Service\MindbodySOAPRequest\Request\SaleService\GetServicesRequest;
+use MiguelAlcaino\MindbodyPaymentsBundle\Service\MindbodySOAPRequest\Request\SaleService\ShoppingCart\CartItemRequest;
 use MiguelAlcaino\MindbodyPaymentsBundle\Service\MindbodySOAPRequest\SaleServiceSOAPRequest;
 
 class SaleServiceRequestHandler
@@ -52,5 +54,26 @@ class SaleServiceRequestHandler
         );
 
         return $formattedServices;
+    }
+
+    /**
+     * @param string            $clientId
+     * @param CartItemRequest[] $cartItems
+     *
+     * @return array
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function calculateShoppingCart(string $clientId, array $cartItems): array
+    {
+        $checkoutShoppingCartRequest = new CheckoutShoppingCartRequest(
+            $clientId,
+            $cartItems,
+            true
+        );
+        $checkoutShoppingCartRequest
+            ->setInStore(true)
+            ->setFields(['paymentcheck']);
+
+        return $this->saleServiceSOAPRequest->checkoutShoppingCart($checkoutShoppingCartRequest);
     }
 }
